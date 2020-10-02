291ae7b6e6c0d7d5e4ba7badaa9c2de1adaca7ea
Jim Stagg <jstagg@gmail.com>
2020-10-02 15:11:24 -0400

    Initial push

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OuchThatIsSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string v;
+            try
+            {
+                if (args.Length > 0)
+                {
+                    v = (args[0]);
+                }
+
+                else
+                {
+                    v = "NOINPUT";
+                }
+                Console.WriteLine($"Hello {v}, Welcome To The World!");
+                Console.WriteLine($"Your name has {v.Length} letters in it!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e}");
+            }
+        }
+    }
+}
